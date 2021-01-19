24a81835af07c9dee02c8769b31fe91b763cd572
data-miner00 <data-miner00@users.noreply.github.com>
2021-01-19 23:41:34 +0800

    feat: Added assignment page

---
+++ src/components/AssignmentItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <div class="horizontal-group">
+      <div class="course-code">{{ courseCode }}</div>
+      <div class="horizontal-group-2">
+        <div class="course-name">{{ courseName }}</div>
+        <div class="assign-no">{{ assignNo }}</div>
+      </div>
+    </div>
+    <div class="link-icon">
+      <v-icon>mdi-star-four-points</v-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    courseCode: String,
+    courseName: String,
+    assignNo: String,
+    assignDescription: String,
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.wrapper
+  padding: 15px
+  border-bottom: 1px solid #eee
+  display: flex
+  align-items: center
+  justify-content: space-between
+
+  .horizontal-group
+
+    .course-code
+      font-weight: 600
+      font-size: 13px
+
+    .horizontal-group-2
+      display: flex
+      align-items: center
+      .assign-no
+        font-size: 8px
+        padding: 3px 10px
+        background: #eee
+        border-radius: 25px
+        margin-left: 5px
+</style>
---
+++ src/views/student/Assignments.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="assignments">
+    <div class="page-description">
+      The below are the assignments of the enrolled courses that are available
+      for this semester. To direct to the workspace for the assignment, click on
+      the button. To navigate to the assignment and group details, click on the
+      star button.
+    </div>
+    <div class="section-divider"></div>
+    <AssignmentItem
+      courseCode="UECS1234"
+      courseName="Advanced Quantum Mechanics"
+      assignNo="1"
+    />
+    <AssignmentItem
+      courseCode="UECS1234"
+      courseName="Advanced Quantum Mechanics"
+      assignNo="2"
+    />
+    <AssignmentItem
+      courseCode="UECS1234"
+      courseName="Advanced Quantum Mechanics"
+      assignNo="3"
+    />
+  </div>
+</template>
+
+<script>
+import AssignmentItem from "@/components/AssignmentItem";
+export default {
+  components: {
+    AssignmentItem,
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.assignments
+  .page-description
+    padding: 15px
+    border-bottom: 1px solid #eee
+  .section-divider
+    background: #f7f9fa
+    height: 10px
+    border-bottom: 1px solid #eee
+</style>
