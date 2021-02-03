5dad831c9f3aacffc819af6bb38bc2b10ef13195
data-miner00 <data-miner00@users.noreply.github.com>
2021-02-03 16:43:27 +0800

    fix: Fixed wrong endpoint

--- src/config.js
+++ src/config.js
@@ -1,15 +1,9 @@
-import assert from "assert";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const { PORT, HOST, HOST_URL } = process.env;
-
-assert(PORT, "PORT is required");
-assert(PORT, "HOST is required");
+const { HOST_URL } = process.env;
 
 export default {
-  port: PORT,
-  host: HOST,
   url: HOST_URL,
 };
