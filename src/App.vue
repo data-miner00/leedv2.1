04f0603ac32942b170a9cea7504410911e5d543b
data-miner00 <data-miner00@users.noreply.github.com>
2021-02-23 03:04:22 +0800

    refactor: Migrate global style to dedicated file

--- src/App.vue
+++ src/App.vue
@@ -16,19 +16,5 @@ export default {
 
 <!-- Global Styling -->
 <style lang="sass">
-.section-divider
-  background: #f7f9fa
-  height: 10px
-  border-bottom: 1px solid #eee
-
-.page-description
-  padding: 15px
-  border-bottom: 1px solid #eee
-  max-width: 689px
-
-textarea
-  outline: none
-
-a
-  text-decoration: none
+@import "./assets/sass/fairshare"
 </style>
