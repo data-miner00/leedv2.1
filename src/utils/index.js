4e74a7f95477d3194d6114b9a7fecf4c4a80a00b
data-miner00 <data-miner00@users.noreply.github.com>
2021-03-04 16:57:20 +0800

    feat: Added 2 util methods

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,31 @@ export const snippet = (string, stopAt) => {
   if (stopAt) number = stopAt;
   return string.slice(0, stopAt) + "...";
 };
+
+// Helper
+export const alias = (userid) => {
+  return `@${userid}`;
+};
+
+export const range = function(start, stop, step) {
+  if (typeof stop == "undefined") {
+    // one param defined
+    stop = start;
+    start = 0;
+  }
+
+  if (typeof step == "undefined") {
+    step = 1;
+  }
+
+  if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
+    return [];
+  }
+
+  var result = [];
+  for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
+    result.push(i);
+  }
+
+  return result;
+};
