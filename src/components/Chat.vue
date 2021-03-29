39e041a67e643cc28e9727e27ebde0572c5817a6
data-miner00 <data-miner00@users.noreply.github.com>
2021-03-29 17:13:13 +0800

    feat: Added more processings for chat

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="right">
       <div class="definition">
-        <div class="name">{{ username }}</div>
+        <div class="name">{{ shortenedName }}</div>
         <div class="time">{{ getTimestamp }}</div>
       </div>
       <div class="message">
@@ -21,11 +21,65 @@ export default {
     avatarUri: String,
     username: String,
     message: String,
-    timestamp: String,
+    timestamp: Object,
+  },
+  methods: {
+    timestampGetDateTime({ seconds, nanoseconds }) {
+      const today = new Date();
+      const date = new Date(seconds * 1e3 + nanoseconds / 1e6);
+
+      const diffTime = Math.abs(today - date);
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+      if (diffDays == 0) {
+        return `Today at ${date
+          .getHours()
+          .toString()
+          .padStart(2, "0")}:${date
+          .getMinutes()
+          .toString()
+          .padStart(2, "0")}`;
+      } else if (diffDays == 1) {
+        return `Yesterday at ${date
+          .getHours()
+          .toString()
+          .padStart(2, "0")}:${date
+          .getMinutes()
+          .toString()
+          .padStart(2, "0")}`;
+      } else {
+        return `${this.getStringMonth(
+          date.getMonth()
+        )} ${date.getdate()} at ${date
+          .getHours()
+          .toString()
+          .padStart(2, "0")}:${date
+          .getMinutes()
+          .toString()
+          .padStart(2, "0")}`;
+      }
+    },
+    getStringMonth(index) {
+      const months = [
+        "January",
+        "Febrauary",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      return months[index];
+    },
   },
   computed: {
     getTimestamp() {
-      if (this.timestamp) return this.timestamp;
+      if (this.timestamp) return this.timestampGetDateTime(this.timestamp);
       else {
         const current = new Date();
         const currentHour = current.getHours();
@@ -34,6 +88,9 @@ export default {
           .toString()
           .padStart(2, "0")}:${currentMinute.toString().padStart(2, "0")}`;
       }
+    },
+    shortenedName() {
+      return this.username.split(" ")[0];
     },
   },
 };
