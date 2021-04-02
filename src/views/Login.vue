5efb59dc9297d77a66740c55b0522892595682cf
data-miner00 <data-miner00@users.noreply.github.com>
2021-04-02 18:10:28 +0800

    style: Removed unused comments

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -34,29 +34,6 @@
       .link Side Note
       .link Settings
       .link &copy; 2021 Leed Pte Ltd.
-
-    //- <!-- <v-text-field
-    //-   v-model="userId"
-    //-   prepend-icon="person"
-    //-   name="login"
-    //-   label="Login"
-    //- ></v-text-field>
-    //- <v-text-field
-    //-   v-model="password"
-    //-   prepend-icon="lock"
-    //-   name="password"
-    //-   label="Password"
-    //- ></v-text-field>
-
-    //- <v-alert color="error" v-if="isError" icon="close">
-    //-   The username or passowrd are incorrect
-    //- </v-alert>
-
-    //- <v-btn rounded color="primary" dark @click.prevent="login">
-    //-   Login
-    //-   <v-icon>keyboard_arrow_right</v-icon>
-    //- </v-btn> -->
-  
 </template>
 
 <script>
