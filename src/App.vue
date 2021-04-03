232f681d1e34cbd2440eaafc35f52915c06f92e0
data-miner00 <data-miner00@users.noreply.github.com>
2021-04-03 11:09:40 +0800

    feat: Global snackbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <v-app>
+    <!-- Global Snackbar -->
+    <v-snackbar v-model="show" :color="color" outlined>
+      {{ text }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn :color="color" text v-bind="attrs" @click="show = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+
+    <!-- Entry Point -->
     <router-view></router-view>
   </v-app>
 </template>
@@ -9,8 +21,19 @@ export default {
   name: "App",
 
   data: () => ({
-    //
+    show: false,
+    text: "A friendly notifier",
+    color: "rgb(121, 75, 196)",
   }),
+  created() {
+    this.$store.subscribe((mutation, state) => {
+      if (mutation.type === "showMessage") {
+        this.text = state.snackbar.text;
+        this.color = state.snackbar.color;
+        this.show = true;
+      }
+    });
+  },
 };
 </script>
 
