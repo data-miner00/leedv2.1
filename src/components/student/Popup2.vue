4b4df216d528d88c9f469bd89e6505c9265a2b5a
data-miner00 <data-miner00@users.noreply.github.com>
2021-04-08 23:03:52 +0800

    fix: Error message shown!

--- src/components/student/Popup2.vue
+++ src/components/student/Popup2.vue
@@ -3,6 +3,7 @@
     v-card.px-4
       v-card-title Create or Join a Group
       v-card-text
+
         .alignment
           .join
             input.group-id-input(
@@ -50,12 +51,16 @@ export default {
     async join() {
       // Removing try clause to get the response status
 
-      const res = await axios.post("group/join", {
-        studentId: this.userId,
-        groupId: this.groupIdInput,
-        email: this.email,
-        assignmentId: this.assignmentId,
-      });
+      const res = await axios.post(
+        "group/join",
+        {
+          studentId: this.userId,
+          groupId: this.groupIdInput,
+          email: this.email,
+          assignmentId: this.assignmentId,
+        },
+        { validateStatus: false }
+      );
 
       if (res.status == 200) {
         this.reset();
@@ -70,6 +75,10 @@ export default {
       } else if (res.status == 404) {
         this.errored = true;
         this.errorMessage = "Sorry, the id does not belong to any group.";
+      } else if (res.status == 400) {
+        this.errored = true;
+        this.errorMessage =
+          "Sorry, the group does not belong to the same assignment.";
       }
     },
     async create() {
@@ -86,17 +95,18 @@ export default {
       }
     },
     async matchmake() {
-      const res = await axios.post("group/matchmake", {
-        studentId: this.userId,
-        assignmentId: this.assignmentId,
-        assignNo: this.assignNo,
-        email: this.email,
-      });
-      if (res.status == 200) {
+      try {
+        await axios.post("group/matchmake", {
+          studentId: this.userId,
+          assignmentId: this.assignmentId,
+          assignNo: this.assignNo,
+          email: this.email,
+        });
+
         console.log("Successfully placed in queue.");
         this.$emit("done");
-      } else if (res.status == 400) {
-        console.log("Something failed");
+      } catch (error) {
+        console.error(error);
       }
     },
     reset() {
@@ -146,6 +156,7 @@ export default {
   border-radius: 5px
 .err-msg
   margin-top: 5px
+  color: crimson
 .matchmake-btn
   width: 204.9px
   height: 40px
