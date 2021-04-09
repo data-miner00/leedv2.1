7c0c4473a691f5581f3f6473d1cfdbe6f94d61c0
data-miner00 <data-miner00@users.noreply.github.com>
2021-04-10 01:02:27 +0800

    test: Added lifecycle test

---
+++ tests/unit/lifecycle.spec.js
@@ -0,0 +1,30 @@
+import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
+import Vuetify from "vuetify";
+
+import NotiItem from "@/components/notification/NotiItem";
+
+describe("Lyfecyele hooks", () => {
+  const localVue = createLocalVue();
+  let vuetify;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+  });
+
+  it("should render lifecycle hooks correctly", () => {
+    const createdSpy = jest.spyOn(NotiItem, "created");
+    const mountedSpy = jest.spyOn(NotiItem, "mounted");
+    const beforeDestroySpy = jest.spyOn(NotiItem, "beforeDestroy");
+
+    const wrapper = shallowMount(NotiItem, {
+      localVue,
+      vuetify,
+    });
+
+    expect(createdSpy).toHaveBeenCalled();
+    expect(mountedSpy).toHaveBeenCalled();
+
+    wrapper.destroy();
+    expect(beforeDestroySpy).toHaveBeenCalled();
+  });
+});
